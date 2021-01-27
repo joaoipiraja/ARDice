eebb8c1bdb84365b752a20e65cfd9743cec941b8
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-01-27 10:02:42 -0300

    Create a horizontal plane for test

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -19,13 +19,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
+        /*
         let diceScene = SCNScene(named: "art.scnassets/dice.scn")!
         
         if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
             diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
             sceneView.scene.rootNode.addChildNode(diceNode)
+            sceneView.autoenablesDefaultLighting = true
         }
+        */
+        
         
         /*
         let sphere = SCNSphere(radius: 0.2)
@@ -55,6 +60,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -77,6 +84,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor{
+            
+            let planeAnchor = anchor as! ARPlaneAnchor
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height:  CGFloat(planeAnchor.extent.z))
+            let planeNode = SCNNode()
+            planeNode.position = SCNVector3(x: planeAnchor.extent.x, y: 0, z: planeAnchor.extent.z)
+            planeNode.transform = SCNMatrix4MakeRotation(Float.pi/2,1,0,0)
+            let gridMaterial = SCNMaterial()
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            node.addChildNode(planeNode)
+            
+            
+            
+        }else{
+            return
+        }
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
